f02ad7474b03e6604bfacd370fcfb29cca0cfb79
faalsh <fhooodi@gmail.com>
2016-09-29 16:02:09 +0200

    createlist tester

--- src/components/Tester.js
+++ src/components/Tester.js
@@ -23,14 +23,23 @@ class Tester extends React.Component {
 		const {boardId, listId} = this.state
 		this.props.actions.addItem(parseInt(boardId), parseInt(listId), {itemId:_.random(1000), itemText:'test '+_.random(100)})
 	}
+	createList(){
+		const {title,boardId} = this.state
+		this.props.actions.createList(parseInt(boardId), title)
+	}
 
     render() {
         return(
         	<div>
         		<div>Add Item: 
-        		boardId<input onChange={this.handleChange.bind(this,'boardId')} />
-        		listId<input onChange={this.handleChange.bind(this,'listId')}/>
-        		<button onClick={this.addItem.bind(this)}>Add Item</button>
+	        		boardId<input onChange={this.handleChange.bind(this,'boardId')} />
+	        		listId<input onChange={this.handleChange.bind(this,'listId')}/>
+	        		<button onClick={this.addItem.bind(this)}>Add Item</button>
+        		</div>
+        		<div>Create list: 
+	        		boardId<input onChange={this.handleChange.bind(this,'boardId')} />
+	        		title<input onChange={this.handleChange.bind(this,'title')}/>
+	        		<button onClick={this.createList.bind(this)}>Create List</button>
         		</div>
         	</div>
         )
