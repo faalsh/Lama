4e97d5a8498a17ec282e4c28e47671ccd9a6125d
faalsh <fhooodi@gmail.com>
2016-09-30 14:33:33 +0200

    prod server

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var express = require('express');
+var fallback = require('express-history-api-fallback');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
+var app = express();
 
-  console.log('Listening at http://localhost:3000/');
+app.set('port', (process.env.PORT || 5000));
+var root = `${__dirname}`;
+
+app.use(express.static(root));
+app.use(fallback('index.html',  {root} ));
+
+
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
