c77a8faf9e9e26cd2ce75aa4e1641e73a65b94c1
faalsh <fahad@Fahads-MacBook-Pro.local>
2016-10-01 18:27:13 +0200

    added board title

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,18 +6,27 @@ import _ from 'lodash'
 export default class Board extends Component {
 
   render() {
-  	const {lists,boardId} = this.props.board
-    const sortedLists = _.sortBy(lists,'listIndex').map(list => 
+  	const {lists,boardId,boardTitle} = this.props.board
+    const sortedLists = _.sortBy(lists,'listIndex').map(list =>
         <List key={list.listId} items={_.sortBy(list.items, 'itemIndex')} title={list.listTitle} listId={list.listId} boardId={boardId}/>)
   	const style = {
   		display: 'flex',
       marginTop: '10px'
   	}
+    const titleStyle = {
+      marginTop: '10px',
+      marginLeft: '5px',
+      fontWeight: 'bold',
+      color: 'rgb(65, 89, 125)'
+    }
     return (
-		<div style={style}>
-			{sortedLists}
-      <CreateList  boardId={boardId}/>
-		</div>
+      <div>
+        <div style={titleStyle}>{boardTitle}</div>
+    		<div style={style}>
+    			{sortedLists}
+          <CreateList  boardId={boardId}/>
+    		</div>
+      </div>
     );
   }
 }
