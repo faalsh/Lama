9b4d545fb93fc26ad144876f7073a542b0941507
faalsh <fahad@Fahads-MacBook-Pro.local>
2017-03-12 18:09:08 +0300

    removed react perf

--- src/web/index.js
+++ src/web/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './containers/App'
 import {Provider} from 'react-redux'
 import store from '../common/store'
-import ReactPerf from 'react/lib/ReactPerf'
+// import ReactPerf from 'react/lib/ReactPerf'
 
-window.ReactPerf = ReactPerf
+// window.ReactPerf = ReactPerf
 console.time('initial')
 ReactDOM.render(
   <Provider store={store}>
